676165469efd488fbfcf279a818dd0ea7d35d5b6
ayPromise servant <fatherpother@gmail.com>
2024-12-05 06:56:52 +0200

    added font to the component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Figtree } from "next/font/google"
+
+const figtree = Figtree({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <main className={figtree.className}>
+    <Component {...pageProps} />
+  </main>;
 }
