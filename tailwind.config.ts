9129704f3acca041a3e38a38def3b6a114b25ad4
ayPromise servant <fatherpother@gmail.com>
2024-12-05 06:55:54 +0200

    implemented design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightYellow:'#F4D04E',
+        darkGray:'#111111',
+        lightGray:'#6B6B6B'
       },
+      spacing:{
+        '50':'4px',
+        '100':'8px',
+        '150':'12px',
+        '300':'24px'
+      },
+      boxShadow:{
+        custom:'8px 8px 0px 0px rgba(0,0,0,0)'
+      }
     },
   },
   plugins: [],
